8aceff1458c78b9b94386193c0107087d31dcb8d
GuidoCh23 <guidochipana23@gmail.com>
2025-06-10 23:52:45 -0500

    test(helpers): agrega tests para GestorArchivos
    Se agregaron tests en test_helpers.py para los metodos de la clase GestorArchivos, se prueba tanto en casos de exito y fallo

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,6 +1,7 @@
 import os
 import pytest
 from src.helpers.gestor_directorios import GestorDirectorios
+from src.helpers.gestor_archivos import GestorArchivos
 
 
 def test_gestor_directorios_crea_directorio():
@@ -24,3 +25,93 @@ def test_gestor_directorios_crea_directorio_cadena_vacia():
     # act y assert
     with pytest.raises(FileNotFoundError):
         gestorDirectorios.crear_directorio()
+
+
+def test_gestor_archivos_escribir_archivo():
+    # arrange
+    gestorArchivos = GestorArchivos()
+
+    # act
+    gestorArchivos.escribir_archivo("texto.txt", "Este es un texto de prueba")
+    contenido = gestorArchivos.leer_archivo("texto.txt")
+
+    # assert
+    assert contenido == "Este es un texto de prueba"
+
+    # limpiamos
+    os.remove("texto.txt")
+
+
+def test_gestor_archivos_escribir_archivo_ruta_carpeta_vacia():
+    # arrange
+    gestorArchivos = GestorArchivos()
+
+    # act y assert
+    with pytest.raises(FileNotFoundError):
+        gestorArchivos.escribir_archivo("", "Este es un texto de prueba")
+
+
+def test_gestor_archivos_crear_archivo_exitoso():
+    # arrange
+    gestorArchivos = GestorArchivos()
+
+    # act
+    gestorArchivos.crear_archivo("texto.txt")
+
+    # assert
+    assert gestorArchivos.existe_archivo("texto.txt")  # True
+
+    # limpiamos
+    os.remove("texto.txt")
+
+
+def test_gestor_archivos_crear_archivo_fallido():
+    # arrange
+    gestorArchivos = GestorArchivos()
+    gestorArchivos.crear_archivo("texto.txt")
+
+    # act y arrange
+    with pytest.raises(FileExistsError):
+        gestorArchivos.crear_archivo("texto.txt")
+
+
+def test_gestor_archivos_leer_archivos_exitoso():
+    # arrange
+    gestorArchivos = GestorArchivos()
+    contenido = "hola, este es un contenido de prueba"
+    gestorArchivos.escribir_archivo("texto.txt", contenido)
+
+    # act y assert
+    assert gestorArchivos.leer_archivo("texto.txt") == contenido
+
+    # limpiamos
+    os.remove("texto.txt")
+
+
+def test_gestor_archivos_leer_archivos_fallido():
+    # arrange
+    gestorArchivos = GestorArchivos()
+
+    # act y assert
+    with pytest.raises(FileNotFoundError):
+        gestorArchivos.leer_archivo("texto.txt")
+
+
+def test_gestor_archivos_existe_archivo_verdadero():
+    # arrange
+    gestorArchivos = GestorArchivos()
+    gestorArchivos.crear_archivo("texto.txt")
+
+    # act y assert
+    assert gestorArchivos.existe_archivo("texto.txt")  # True
+
+    # limpiamos
+    os.remove("texto.txt")
+
+
+def test_gestor_archivos_existe_archivo_falso():
+    # arrange
+    gestorArchivos = GestorArchivos()
+
+    # act y assert
+    assert not gestorArchivos.existe_archivo("texto.txt")  # False
